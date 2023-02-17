49940d8cfe1c3d80ddce6a1bb60c9ee68475bc82
Murilo Menezes <murilomenezes@Murilos-MacBook-Pro-2.local>
2023-02-17 19:50:55 -0300

    restrict bot to PM only

--- bot.py
+++ bot.py
@@ -13,24 +13,26 @@ intents.members = True
 client = discord.Client(intents=intents)
 
 
-@client.event
-async def on_ready():
-    guild = discord.utils.get(client.guilds, name='TEST SERVER')	
-    channel = discord.utils.get(guild.text_channels, name='general')
-    await channel.send('O bot está online!')
+# @client.event
+# async def on_ready():
+#     guild = discord.utils.get(client.guilds, name='TEST SERVER')	
+#     channel = discord.utils.get(guild.text_channels, name='general')
+#     await channel.send('O bot está online!')
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.lower() == "!help":
-    	await message.channel.send('Olá! Digite !source para acessar meu source code, e !author para saber sobre meu criador.')
+	if not isinstance(message.channel, discord.DMChannel):
+		return # ignore messages that are not sent via private message
+	if message.author == client.user:
+		return
 
-    if message.content.lower() == '!source':
-        await message.channel.send('Olá! Você pode encontrar meu source code em https://github.com/murilomenezes1/DiscordBot')
-    if message.content.lower() == "!author":
-    	await message.channel.send("Fui desenvolvido por Murilo Menezes para a disciplina de NLP!")
+	if message.content.lower() == "!help":
+		await message.channel.send('Olá! Digite !source para acessar meu source code, e !author para saber sobre meu criador.')
+	if message.content.lower() == '!source':
+		await message.channel.send('Olá! Você pode encontrar meu source code em https://github.com/murilomenezes1/DiscordBot')
+	if message.content.lower() == "!author":
+		await message.channel.send("Fui desenvolvido por Murilo Menezes para a disciplina de NLP!")
 
 client.run(str(TOKEN))
 
